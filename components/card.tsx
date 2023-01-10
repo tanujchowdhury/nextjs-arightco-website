e183d21467a3876484d4b4830bf978dd16076b4d
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-09 17:38:12 -0800

    responsive cards

--- components/card.tsx
+++ components/card.tsx
@@ -62,18 +62,29 @@ export default function Card({
     return (
       <motion.div
         whileHover={{
-          y: -20,
+          
         }}
         transition={{
           duration: 0.2,
         }}
-        className="flex flex-col items-center justify-center rounded-lg shadow-md text-center p-2 m-2 gap-3 w-full h-full group hover:bg-gradient-to-r hover:from-blue hover:to-blue-light"
+        className="flex flex-col items-center justify-center rounded-lg shadow-md text-center p-5 m-1 gap-3 w-full h-full group bg-white -translate-y-32"
       >
-        <div className="w-[10vh] h-[10vh] text-blue">{children}</div>
-        <div className="text-blue-dark font-bold lg:text-2xl group-hover:text-white">
+        <div className="w-[10vh] h-[10vh] text-blue group-hover:text-orange-dark">
+          {children}
+        </div>
+        <div className="text-blue-dark font-bold lg:text-2xl">
           {title}
         </div>
-        <div className="text-sm lg:text-base group-hover:text-white"><p>{text[0]}<span className="text-orange-dark font-bold">{text[1]}</span>{text[2]}</p></div>
+        <div className="text-sm lg:text-base">
+          <p>
+            {text[0]}
+            <span className="text-orange-dark font-bold">{text[1]}</span>
+            {text[2]}
+          </p>
+        </div>
+        <button className="h-12 w-40 rounded-full text-white font-bold bg-gradient-to-r from-orange-dark to-orange-light hover:from-blue-dark hover:to-blue-light cursor-pointer">
+          LEARN MORE
+        </button>
       </motion.div>
     );
   }
