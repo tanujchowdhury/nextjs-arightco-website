ba0f4067f528e057797d089edfbe606479cdc3cc
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-11 16:48:30 -0800

    carousel buttons

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -7,7 +7,7 @@ function SampleNextArrow(props: { className: any; style: any; onClick: any }) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "black" }}
+      style={{ ...style, display: "block", background: "black", borderRadius: "100%",}}
       onClick={onClick}
     />
   );
@@ -18,7 +18,7 @@ function SamplePrevArrow(props: { className: any; style: any; onClick: any }) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "black" }}
+      style={{ ...style, display: "block", background: "black", borderRadius: "100%", }}
       onClick={onClick}
     />
   );
