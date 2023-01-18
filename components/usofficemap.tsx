e288338b37f3fc8dc720d4cfeb7a20f0580d5fd0
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-17 18:57:05 -0800

    add map

--- components/usofficemap.tsx
+++ components/usofficemap.tsx
@@ -2,14 +2,21 @@ import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function USOfficeMap() {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ? process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY : "",
-      });
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+      ? process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+      : "",
+  });
 
-    if (!isLoaded) return <div className="bg-white p-10 m-5 mt-20 rounded-3xl">Loading...</div>
+  if (!isLoaded)
     return (
-        
-            <GoogleMap zoom={10} center={{lat: 37, lng: -122}} mapContainerClassName="bg-white p-10 m-5 mt-20 rounded-3xl h-96 w-auto"></GoogleMap>
-       
+      <div className="bg-white p-10 m-5 mt-20 rounded-3xl">Loading...</div>
     );
+  return (
+    <GoogleMap
+      zoom={10}
+      center={{ lat: 44, lng: -80 }}
+      mapContainerClassName="bg-white p-10 m-5 mt-20 rounded-3xl h-96 w-auto"
+    ></GoogleMap>
+  );
 }
