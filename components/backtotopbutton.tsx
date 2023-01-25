c39df35d80020d6ccd6cba4d49e9a0f0c5133697
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-25 13:49:29 -0800

    back to top button responsiveness

--- components/backtotopbutton.tsx
+++ components/backtotopbutton.tsx
@@ -6,27 +6,31 @@ export default function BackToTopButton() {
   const [button, setButton] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 1000) {
+      if (window.scrollY > 700) {
         setButton(true);
       } else {
         setButton(false);
       }
     });
   }, []);
-const scrollUp = () => {
+  const scrollUp = () => {
     window.scrollTo({
-        top: 400,
-        behavior: "smooth"
-    })
-}
+      top: 400,
+      behavior: "smooth",
+    });
+  };
 
-  return <div>
-    {button && (
-        <button
-        onClick={scrollUp}
-         className="fixed bottom-1 right-1 sm:bottom-2 sm:right-2 md:bottom-4 md:bottom-4 lg:bottom-8 lg:right-8 xl:bottom-12 xl:right-12 w-16 h-12 flex flex-row items-center justify-center text-xl text-orange bg-white hover:text-blue-light hover:bg-orange-light z-10 border border-orange rounded px-1">Top
-            <FontAwesomeIcon icon={faTurnUp} className="w-4 h-4" />
-        </button>
-    )}
-  </div>;
+  return (
+    <div>
+      {button && (
+        <div
+          onClick={scrollUp}
+          className="fixed bottom-1 right-1 sm:bottom-2 sm:right-2 md:bottom-4 md:bottom-4 lg:bottom-8 lg:right-8 xl:bottom-12 xl:right-12 flex flex-row items-center justify-center text-xl text-orange bg-white hover:text-blue-light hover:bg-orange-light z-10 border border-orange rounded p-1 cursor-pointer"
+        >
+          <div className="hidden md:block">Top</div>
+          <FontAwesomeIcon icon={faTurnUp} className="w-4 h-4" />
+        </div>
+      )}
+    </div>
+  );
 }
