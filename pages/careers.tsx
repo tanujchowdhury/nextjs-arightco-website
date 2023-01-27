c31ddc6da43adbb56e91f55f78335d0787519eb4
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-27 13:43:32 -0800

    careers page

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import JobCard from "../components/cards/jobcard";
 import Layout from "../components/layout";
 import Section from "../components/sections/section";
 
@@ -16,6 +17,13 @@ export default function Careers() {
         linktext={"hr@arightco.com"}
         href={"mailto:hr@arightco.com"}
       />
+      <div className="flex flex-col items-start justify-start bg-white my-20 mx-3 xl:mx-32 p-8 rounded-3xl">
+        <div className="border-b border-gray-light w-full text-5xl py-4 font-bold">Job Postings</div>
+        <div>
+          <JobCard jobTitle={"Accountant"} jobCategory={"Finance & Accounting"} jobType={"Freelance, Full Time, Part Time"} jobLocation={"San Jose"} href={"/jobs/accountant"} />
+          <JobCard jobTitle={"Human Resources"} jobCategory={"Human Resources"} jobType={"Freelance, Full Time, Part Time"} jobLocation={"San Jose"} href={"/jobs/human-resources"} />
+        </div>
+      </div>
     </Layout>
   );
 }
