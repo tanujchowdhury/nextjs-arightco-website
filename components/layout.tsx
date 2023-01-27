08e7401395c5a4870370ffb0fce60eb08a783e66
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-26 16:02:44 -0800

    backend set up

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,6 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div>
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
+        <meta name="robots" content="all" />
       </Head>
       <Navbar />
       <main>{children}</main>
