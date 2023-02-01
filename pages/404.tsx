7b40379dc085a6e3f8549ed3e5ff29aa7bf9c50a
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-01-31 23:41:17 -0800

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Layout from "../components/layouts/layout";
+import Section from "../components/sections/section";
+
+export default function Thanks() {
+  return (
+    <Layout>
+      <Head>
+        <title>404 Page Not Found | ArightCo</title>
+      </Head>
+      <Section title1={"404"} text={"We couldn't find the page you looking for."} type={3} />
+    </Layout>
+  );
+}
