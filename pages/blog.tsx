176d34d09abb7de56ed69a93bff71b00b38d3877
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-02-03 13:21:09 -0800

    add initial blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layouts/layout";
+
+export default function Blog() {
+    return (
+        <Layout>
+            <div className="m-96">
+
+            Enter
+            </div>
+        </Layout>
+    );
+}
