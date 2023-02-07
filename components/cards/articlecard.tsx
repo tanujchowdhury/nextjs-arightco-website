742f1d7a8f5591fbaf3845d96fba4b39b179c785
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-02-06 19:01:12 -0800

    test commit

---
+++ components/cards/articlecard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ArticleCard({
+  src,
+  alt,
+  title,
+  href,
+  date,
+}: {
+  src: string;
+  alt: string;
+  title: string;
+  href: string;
+  date: string;
+}) {
+  
+  return (
+    <Link href={href}>
+      <div className="relative flex flex-col drop-shadow-sm transition duration-300 rounded-md hover:drop-shadow-xl bg-white group">
+        <div className="w-full">
+          <Image src={src} alt={alt} width={500} height={500} className="rounded-t-md" />
+        </div>
+        <div className="absolute top-0 right-0 mt-2 mr-2 p-1 bg-orange rounded-full text-sm text-white">ARTICLE</div>
+        <div className="basis-4/12 border-b-[1px]">
+          <div className="text-xl font-bold text-blue p-3 pb-0 group-hover:text-orange-dark">{title}</div>
+          <div className="text-sm font-bold text-orange-dark p-3">READ MORE...</div>
+        </div>
+        <div className="basis-1/12 text-sm p-2 pl-3">{date}</div>
+      </div>
+    </Link>
+  );
+}
