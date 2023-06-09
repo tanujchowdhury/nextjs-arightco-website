cecea6738c751375c90e2f7e500bfa8dc6038972
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-06-08 23:23:51 -0500

    bug fix

--- components/cards/pricingcard.tsx
+++ components/cards/pricingcard.tsx
@@ -29,46 +29,48 @@ export default function PricingCard({
   };
 
   return (
-    <motion.div variants={variants} initial="hidden" animate="visible">
-      <div className="flex flex-col items-stretch bg-gray-lightest p-3 shadow w-full rounded-lg h-[500px] hover:shadow-xl hover:bg-white">
-        <div className="text-lg font-bold text-white bg-blue-dark text-center py-2 rounded">
-          {title}
-        </div>
-        <div className="text-xl font-semibold text-blue-dark text-center my-2">
-          {price}
-        </div>
-        <div className="border-b border-gray-dark pb-2" />
+    <>
+      <motion.div variants={variants} initial="hidden" animate="visible">
+        <div className="flex flex-col items-stretch bg-gray-lightest p-3 shadow w-full rounded-lg h-[500px] hover:shadow-xl hover:bg-white">
+          <div className="text-lg font-bold text-white bg-blue-dark text-center py-2 rounded">
+            {title}
+          </div>
+          <div className="text-xl font-semibold text-blue-dark text-center my-2">
+            {price}
+          </div>
+          <div className="border-b border-gray-dark pb-2" />
 
-        <div className="flex-grow overflow-auto gap-4 flex flex-col mt-5">
-          {services.map((service: string) => (
-            <div
-              key={service}
-              className="pl-4 text-gray-700 text-md flex items-start"
-            >
-              <FontAwesomeIcon
-                icon={faCheck}
-                className="w-6 h-6 block mr-2 flex-shrink-0"
-              />{" "}
-              <p>{service}</p>
-            </div>
-          ))}
+          <div className="flex-grow overflow-auto gap-4 flex flex-col mt-5">
+            {services.map((service: string) => (
+              <div
+                key={service}
+                className="pl-4 text-gray-700 text-md flex items-start"
+              >
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="w-6 h-6 block mr-2 flex-shrink-0"
+                />{" "}
+                <p>{service}</p>
+              </div>
+            ))}
+          </div>
+          <button
+            onClick={() => (form ? closeForm() : openForm())}
+            className="self-center mt-4 bg-gradient-to-r from-orange-dark to-orange-light hover:from-orange-light hover:to-orange-dark rounded-full text-white font-bold text-sm h-10 w-32 transition-colors duration-200"
+          >
+            SELECT
+          </button>
         </div>
-        <button
-          onClick={() => (form ? closeForm() : openForm())}
-          className="self-center mt-4 bg-gradient-to-r from-orange-dark to-orange-light hover:from-orange-light hover:to-orange-dark rounded-full text-white font-bold text-sm h-10 w-32 transition-colors duration-200"
-        >
-          SELECT
-        </button>
-        <AnimatePresence>
-          {form && (
-            <PricingModal
-              handleClose={closeForm}
-              title={modalTitle}
-              value={value}
-            />
-          )}
-        </AnimatePresence>
-      </div>
-    </motion.div>
+      </motion.div>
+      <AnimatePresence>
+        {form && (
+          <PricingModal
+            handleClose={closeForm}
+            title={modalTitle}
+            value={value}
+          />
+        )}
+      </AnimatePresence>
+    </>
   );
 }
