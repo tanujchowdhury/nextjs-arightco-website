1b5b4bb437ea0369e3af129f68644c1d57054293
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-09-15 15:02:43 -0500

    fix routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import type { AppProps } from "next/app";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Analytics } from "@vercel/analytics/react";
+import { Account } from "../components/intranet/Account.js"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Account>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </Account>
   );
 }
