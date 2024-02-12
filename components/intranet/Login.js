2435ee7496cd8c0ce032bf0cacc646d01d3527f6
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2024-02-12 17:22:49 -0600

    add two login forms

--- components/intranet/Login.js
+++ components/intranet/Login.js
@@ -7,7 +7,6 @@ import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
-import { sweepLeft } from "../animations";
 import { sweepRight } from "../animations";
 
 const Login = ({ onLogin }) => {
@@ -53,7 +52,7 @@ const Login = ({ onLogin }) => {
         />
       </div>
 
-      <div className="p-32 flex flex-row justify-evenly items-center">
+      <div className="py-32 flex flex-row justify-around">
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -62,10 +61,66 @@ const Login = ({ onLogin }) => {
         >
           <form
             onSubmit={onSubmit}
-            className="space-y-8 bg-gradient-to-r from-blue-dark to-blue-light shadow-md p-8 rounded-lg w-[50vw] mt-20 text-orange"
+            className="space-y-8 bg-gradient-to-r from-blue-dark to-blue-light shadow-md p-8 rounded-lg w-[40vw] mt-20 text-orange"
+          >
+            <h3 className="text-center text-3xl font-semibold">Client Login</h3>
+            <div className="space-y-4">
+              <label htmlFor="email" className="block font-medium text-lg">
+                Email
+              </label>
+              <input
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                className="p-2 border border-blue-500 rounded w-full"
+              />
+
+              <div className="relative">
+                <label htmlFor="password" className="block font-medium text-lg">
+                  Password
+                </label>
+                <input
+                  type={isPasswordVisible ? "text" : "password"}
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  className="p-2 border border-blue-500 rounded w-full"
+                />
+                <span
+                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                  className="absolute right-2 top-12 transform -translate-y-1/2 text-xl cursor-pointer text-orange"
+                >
+                  {isPasswordVisible ? (
+                    <FontAwesomeIcon icon={faEye} className="w-6 h-6" />
+                  ) : (
+                    <FontAwesomeIcon icon={faEyeSlash} className="w-6 h-6" />
+                  )}
+                </span>
+                {authFailed && (
+                  <p className="text-red-500 mt-2">
+                    We couldn't verify your account.
+                  </p>
+                )}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="mt-4 w-full py-2 rounded text-xl bg-orange text-white hover:bg-orange-dark transition-colors duration-150"
+            >
+              Log In
+            </button>
+          </form>
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={sweepRight}
+        >
+          <form
+            onSubmit={onSubmit}
+            className="space-y-8 bg-gradient-to-r from-blue-dark to-blue-light shadow-md p-8 rounded-lg w-[40vw] mt-20 text-orange"
           >
             <h3 className="text-center text-3xl font-semibold">
-              Intranet Login
+              Employee Login
             </h3>
             <div className="space-y-4">
               <label htmlFor="email" className="block font-medium text-lg">
@@ -92,9 +147,9 @@ const Login = ({ onLogin }) => {
                   className="absolute right-2 top-12 transform -translate-y-1/2 text-xl cursor-pointer text-orange"
                 >
                   {isPasswordVisible ? (
-                    <FontAwesomeIcon icon={faEye} className="w-6 h-6"/>
+                    <FontAwesomeIcon icon={faEye} className="w-6 h-6" />
                   ) : (
-                    <FontAwesomeIcon icon={faEyeSlash} className="w-6 h-6"/>
+                    <FontAwesomeIcon icon={faEyeSlash} className="w-6 h-6" />
                   )}
                 </span>
                 {authFailed && (
