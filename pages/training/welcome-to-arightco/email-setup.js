85bf8e05faf5f995c977eef4dd33aff800bf5064
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2024-06-26 17:25:38 -0500

    update email-setup.js

--- pages/training/welcome-to-arightco/email-setup.js
+++ pages/training/welcome-to-arightco/email-setup.js
@@ -34,14 +34,12 @@ export default function EmailSetup() {
           <h3 className="text-3xl">Email Setup</h3>
 
           <ol>
-            <li>1. To sign up for Gmail, create a Google Account.</li>
-            <li>2. You can use the username and password to sign into Gmail and other Google products like YouTube, Google Play, and Google Drive.</li>
-            <li>3. Go to the Google Account creation page.</li>
-            <li>4. Follow the steps on the screen to set up your account.</li>
-            <li>5. Use the account you created to sign in to Gmail.</li>
-            <li>6. Select 2-step verification for better security.</li>
-            <li>7. Add phone number to the 2-step verification.</li>
-            <li>8. Add the OTP received in the phone number and verify.</li>
+            <li>1. Accept the link from IT (Charles).</li>
+            <li>2. Follow the steps on the screen to set up your account.</li>
+            <li>3. Use the account you created to sign in to Gmail.</li>
+            <li>4. Select 2-step verification for better security.</li>
+            <li>5. Add phone number to the 2-step verification.</li>
+            <li>6. Add the OTP received in the phone number and verify.</li>
           </ol>
 
           <div className="flex flex-row justify-between">
